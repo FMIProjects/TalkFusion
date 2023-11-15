0faee4aebff60f8f290e770715df98a86d94f973
GabroveanuRazvan <gabroveanu@gmail.com>
2023-11-15 10:38:22 +0200

    Added Channel Model.

---
+++ TalkFusion/TalkFusion/Data/Migrations/20231115083728_AddChannel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalkFusion.Data.Migrations
+{
+    public partial class AddChannel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Channels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Channels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Channels_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Channels_CategoryId",
+                table: "Channels",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Channels");
+        }
+    }
+}
---
+++ TalkFusion/TalkFusion/Models/Channel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalkFusion.Models
+{
+    public class Channel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Channel title is mandatory")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Channel description is mandatory")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Channel user is mandatory")]
+        public int UserId { get; set; }
+    }
+}
