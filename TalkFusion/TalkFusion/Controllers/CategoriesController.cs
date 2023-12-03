b812993f18118359c0fe4fca12908f6dae07a11a
GabroveanuRazvan <gabroveanu@gmail.com>
2023-12-03 16:45:38 +0200

    Crud on categories. Added Categories page button in layout navbar.

--- TalkFusion/TalkFusion/Controllers/CategoriesController.cs
+++ TalkFusion/TalkFusion/Controllers/CategoriesController.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TalkFusion.Data;
+using TalkFusion.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace TalkFusion.Controllers
 {
     public class CategoriesController : Controller
     {
+
+        private readonly ApplicationDbContext db;
+
+        public CategoriesController (ApplicationDbContext context)
+        {
+            this.db = context;
+        }
+
         public IActionResult Index()
         {
+            var categories = from category in db.Categories
+                             select category;
+
+            ViewBag.Categories = categories;
+
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.Message = TempData["message"];
+            }
+
             return View();
         }
     }
