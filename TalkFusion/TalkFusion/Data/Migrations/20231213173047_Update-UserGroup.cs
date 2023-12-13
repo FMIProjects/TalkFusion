3933a751559fded49cda04ee63ea396b93cfbc8f
GabroveanuRazvan <gabroveanu@gmail.com>
2023-12-13 19:31:48 +0200

    Succesfully added isModerator atribute to UserGroup.

---
+++ TalkFusion/TalkFusion/Data/Migrations/20231213173047_Update-UserGroup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalkFusion.Data.Migrations
+{
+    public partial class UpdateUserGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsModerator",
+                table: "UserGroups",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups",
+                columns: new[] { "Id", "UserId", "GroupId", "IsModerator" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups");
+
+            migrationBuilder.DropColumn(
+                name: "IsModerator",
+                table: "UserGroups");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups",
+                columns: new[] { "Id", "UserId", "GroupId" });
+        }
+    }
+}
--- TalkFusion/TalkFusion/Models/UserGroup.cs
+++ TalkFusion/TalkFusion/Models/UserGroup.cs
@@ -8,6 +8,7 @@ namespace TalkFusion.Models
         public int Id { get; set; }
         public string? UserId { get; set; }
         public int? GroupId { get; set; }
+        public bool? IsModerator { get; set; }
         public virtual ApplicationUser? User { get; set; }
         public virtual Group? Group { get; set; }
     }
